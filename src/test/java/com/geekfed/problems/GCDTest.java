d4f3acff6700e4242c8d4f71a8ca90bc5c4f451a
Cody Engel <cengel815@gmail.com>
2016-03-18 23:12:36 -0500

    Updated test cases for GCD
    Updated the test cases for GCD to cover the following scenarios:
    
    - Both params are the same
    - One param is prime
    - One param is multiplum of other
    - Normal case

--- src/test/java/com/geekfed/problems/GCDTest.java
+++ src/test/java/com/geekfed/problems/GCDTest.java
@@ -11,13 +11,45 @@ import static org.junit.Assert.*;
  */
 public class GCDTest {
 
+    // Non-recursive
     @Test
-    public void test_NonRecursiveGCDOf48And18Is6() {
-        assertEquals(6, GCD.nonRecursive(48, 18));
+    public void test_NonRecursiveGCDTrickCase() {
+        assertEquals(13, GCD.nonRecursive(13, 13));
     }
 
     @Test
-    public void test_RecursiveGCDOf48And18Is6() {
-        assertEquals(6, GCD.recursive(48, 18));
+    public void test_NonRecursiveGCDWithPrimeArgument() {
+        assertEquals(1, GCD.nonRecursive(37, 600));
+    }
+
+    @Test
+    public void test_NonRecursiveGCDWithMultiplumOfOther() {
+        assertEquals(10, GCD.nonRecursive(10, 1000));
+    }
+
+    @Test
+    public void test_NonRecursiveGCDNormalCase() {
+        assertEquals(6, GCD.nonRecursive(54, 24));
+    }
+
+    // Recursive
+    @Test
+    public void test_RecursiveGCDTrickCase() {
+        assertEquals(13, GCD.recursive(13, 13));
+    }
+
+    @Test
+    public void test_RecursiveGCDWithPrimeArgument() {
+        assertEquals(1, GCD.recursive(37, 600));
+    }
+
+    @Test
+    public void test_RecursiveGCDWithMultiplumOfOther() {
+        assertEquals(10, GCD.recursive(10, 1000));
+    }
+
+    @Test
+    public void test_RecursiveGCDNormalCase() {
+        assertEquals(6, GCD.recursive(54, 24));
     }
 }
